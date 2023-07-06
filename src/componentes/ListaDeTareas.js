77b158862fbb77f2412ac8c55e52bc4b98ae81fb
Jonathan <jonathangutierrezperez@gmail.com>
2023-07-06 19:29:17 +0200

    añadir y eliminar tarea OK

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -10,7 +10,7 @@ const ListaDeTareas = () => {
 
   const [tareas, setTareas] = useState([]);
 
-  const agregarTarea = tarea => {
+  const agregarTarea = (tarea) => {
 
     console.log(tarea);
     if (tarea.texto.trim()) {
@@ -20,32 +20,42 @@ const ListaDeTareas = () => {
     }
   }
 
+  const eliminarTarea = (id) => {
+    const tareasActualizadas = tareas.filter((tarea) => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  }
+
+  const completarTarea = (id) => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if (tarea.id === id) {
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    })
+    setTareas(tareasActualizadas);
+  };
+
+
 
   return (
 
     <>
       <Formulario onSubmit={agregarTarea} />
       <div className="tareas-lista-contenedor">
-        {
-          tareas.map((tarea) => {
-            <Tareas
-              key={tarea.id}
-              id={tarea.id}
-              texto={tarea.texo}
-              completada={tarea.completada}
-            />
+        {tareas.map(tarea =>
 
-
-          })
-
-
+          <Tareas
+            key={tarea.id}
+            id={tarea.id}
+            texto={tarea.texto}
+            completada={tarea.completada}
+            eliminarTarea={eliminarTarea}
+            completarTarea={completarTarea}
+          />)
 
         }
-
       </div>
     </>
-
-
   );
 };
 
